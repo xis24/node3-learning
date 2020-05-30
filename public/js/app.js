8b66076aca82461cf3e6ecd9073b85bc617cec5c
leo <xiaoxuanshi@gmail.com>
2020-05-30 17:25:17 -0400

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,21 +14,19 @@ weatherform.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          return console.log("error");
-        }
-        messageOne.textContent = data.location;
-        messageTwo.textContent = JSON.stringify(data.forecastData);
-        console.log(
-          data.location,
-          data.address,
-          JSON.stringify(data.forecastData)
-        );
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        return console.log("error");
+      }
+      messageOne.textContent = data.location;
+      messageTwo.textContent = JSON.stringify(data.forecastData);
+      console.log(
+        data.location,
+        data.address,
+        JSON.stringify(data.forecastData)
+      );
+    });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //define path for express config
 const publicDirectoryPath = path.join(__dirname, "../public/");
@@ -103,10 +104,6 @@ app.get("*", (req, res) => {
   });
 });
 
-//app.com
-//app.com/help
-//app.com/about
-
-app.listen(3000, () => {
-  console.log("started");
+app.listen(port, () => {
+  console.log("started" + port);
 });
